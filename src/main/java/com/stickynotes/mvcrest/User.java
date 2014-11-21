56cc08b2fdc25db304f49298fd07b3d959fb8995
varunkakuste <varunkakuste@gmail.com>
2014-11-20 18:59:42 -0800

    Integrated work of all team members On Sticky Notes

---
+++ src/main/java/com/stickynotes/mvcrest/User.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.stickynotes.mvcrest;
+
+/**
+ * @author Varun
+ *
+ */
+public class User {
+	private String userName;
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+}
